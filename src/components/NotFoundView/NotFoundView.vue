1834c3b44a76b3d5b30b1ae99c0f85da99a0b49b
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-04-27 23:19:10 +0300

    Add styling to Not found view

--- src/components/NotFoundView/NotFoundView.vue
+++ src/components/NotFoundView/NotFoundView.vue
@@ -1,4 +1,8 @@
 <template>
-  <h1>Not found 404</h1>
-  <router-link to="/">Return to beginning</router-link>
+  <div class="text-center mt-6">
+    <div class="font-bold text-3xl mb-4">Not found 404</div>
+    <router-link to="/" class="cursor-pointer hover:underline"
+      >Return to beginning</router-link
+    >
+  </div>
 </template>

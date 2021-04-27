48e23c5cae94ac33edbbd9127522f9bdbf903e4d
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-04-27 23:19:22 +0300

    Add Progress bar component

---
+++ src/components/ProgressBar/ProgressBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="relative">
+    <div class="overflow-hidden w-full bg-gray-200 h-2 rounded-md">
+      <div :style="{ width: getWidth + '%' }" class="bg-gray-400 h-2"></div>
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>

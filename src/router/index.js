cab30ab9d1a539396e7ec3fe57fc1c8f38dd9cfb
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-04-27 23:21:12 +0300

    Add register check before entering /results route

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,7 @@ const routes = [
         path: "/results",
         name: "results",
         component: ResultsView,
+        beforeEnter: checkIfRegistered,
     },
     {
         path: "/:catchAll(.*)",

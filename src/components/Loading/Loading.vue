7e2f4ad0094e12c3f0b6dcfe4a319045b0866815
Karlis Gustavs Logins <karlis.gustavs.logins@gmail.com>
2021-04-27 23:18:41 +0300

    Add Loading component

---
+++ src/components/Loading/Loading.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="font-bold text-center mt-6 text-xl">Loading...</div>
+  <!-- Room for improvement -->
+</template>
+
+<script>
+export default {
+  name: "Loading",
+};
+</script>
